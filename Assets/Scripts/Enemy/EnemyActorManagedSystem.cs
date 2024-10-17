38210b18e2601986effa266f9df5fd67cd22c28a
Francisco Ramos <blbramos@gmail.com>
2024-10-17 19:29:47 -0400

    .

--- Assets/Scripts/Enemy/EnemyActorManagedSystem.cs
+++ Assets/Scripts/Enemy/EnemyActorManagedSystem.cs
@@ -1,3 +1,4 @@
+using Rukhanka;
 using Sandbox.Player;
 using Unity.Burst;
 using Unity.Collections;
@@ -23,20 +24,21 @@ namespace Enemy
         // so we do not add the [BurstCompile] attribute.
         public void OnUpdate(ref SystemState state)
         {
-            var zone = Animator.StringToHash("Zone");
+            //var zone = Animator.StringToHash("Zone");
             var velz = Animator.StringToHash("velz");
             var Aim = Animator.StringToHash("Aim");
             var combatAction = Animator.StringToHash("CombatAction");
+            var  zone = new FastAnimatorParameter("Zone");
+            
 
-
-            foreach (var (enemyState, enemyMove, actor, entity) in
-                     SystemAPI.Query<RefRO<EnemyStateComponent>, RefRO<EnemyMovementComponent>, ActorInstance>()
+            foreach (var (anim, enemyState, enemyMove, actor, entity) in
+                     SystemAPI.Query<AnimatorParametersAspect, RefRO<EnemyStateComponent>, RefRO<EnemyMovementComponent>, ActorInstance>()
                          .WithEntityAccess())
             {
                 var animator = actor.actorPrefabInstance.GetComponent<Animator>();
                 animator.speed = enemyMove.ValueRO.animatorSpeed;
-                animator.SetInteger(zone, animator.GetInteger(combatAction) > 0 ? 3 : enemyState.ValueRO.Zone);
-                animator.SetInteger(zone, enemyState.ValueRO.Zone);
+                //animator.SetInteger(zone, enemyState.ValueRO.Zone);
+                anim.SetIntParameter(zone, enemyState.ValueRO.Zone);
                 animator.SetFloat(velz, enemyMove.ValueRO.forwardVelocity, enemyMove.ValueRO.blendSpeed,
                     SystemAPI.Time.DeltaTime);
             }
